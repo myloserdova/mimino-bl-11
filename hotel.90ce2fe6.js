d32f92915b10ee9ec8fa258800cc353c8719174a
myloserdova <112957753+myloserdova@users.noreply.github.com>
2022-11-24 11:47:19 +0000

    Deploying to gh-pages from @ myloserdova/mimino-bl-11@c7a0abc6c4aac1a30ead858fc7a4928ede5dc537 🚀

---
+++ hotel.90ce2fe6.js
@@ -0,0 +1,2 @@
+$(".room-time").slick({prevArrow:'<button type="button" class="my-slick-prev-room"></button>',nextArrow:'<button type="button" class="my-slick-next-room"></button>',infinite:!0,speed:300,slidesToShow:1,adaptiveHeight:!0});
+//# sourceMappingURL=hotel.90ce2fe6.js.map
