ea68e0b07f7f61e270ec261223950ca658035c92
addaviktory <112877379+addaviktory@users.noreply.github.com>
2022-12-02 21:08:25 +0200

    base functionality gallery

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-import './js/slick-for-hero';
-import './js/slick-img-rooms';
-import './js/slick-rest-home';
+// import './js/slick-for-hero';
+// import './js/slick-img-rooms';
+// import './js/slick-rest-home';
+import './js/slider-gallery-hotel';
