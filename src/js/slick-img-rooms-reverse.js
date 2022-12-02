3ed3c3d15bc83dd79c5cb9fa7f9a39fe1c4c3e50
George Artemov <oscurasgeorge@gmail.com>
2022-12-02 16:37:02 +0000

    готель-кімнати-десктоп+скролл
    Міррор стрілки, десктоп версія, адаптив галереї мобайл.

---
+++ src/js/slick-img-rooms-reverse.js
@@ -0,0 +1,10 @@
+$('.room-gallery-mirror').slick({
+  // dots: true,
+  prevArrow: '<button type="button" class="my-slick-prev-hotel-reverse"></button>',
+  nextArrow: '<button type="button" class="my-slick-next-hotel-reverse"></button>',
+  infinite: true,
+  speed: 300,
+  slidesToShow: 2,
+  adaptiveHeight: true,
+  variableWidth: true
+});
