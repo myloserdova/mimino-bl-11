300d6353103d2ad7044ab86a7acc48033b4b2bcd
addaviktory <112877379+addaviktory@users.noreply.github.com>
2022-12-02 21:49:14 +0200

    delete old js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-// import './js/slick-for-hero';
-// import './js/slick-img-rooms';
-// import './js/slick-rest-home';
-import './js/slider-gallery-hotel';
+import './js/slick-for-hero';
+import './js/slick-img-rooms';
+import './js/slick-rest-home';
