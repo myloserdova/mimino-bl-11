8e2806e5252136ee499f53ad274032d93365bb74
Inna Pokydko <inna.pokydko@gmail.com>
2022-12-03 22:56:39 +0200

    Restaurant Homepage
    Майже готова, ще підправлю деякі параметри. Прохання до Юри, Георгія, Ельвіри: допоможіть розібратись - на 1024 і 1280 після футера з"являється біла полоса, так як на моб немає, то підозрюю, що вона з"явилась після мого позиціонування, але сама вирішити не можу(

--- src/js/slick-rest-home.js
+++ src/js/slick-rest-home.js
@@ -4,16 +4,7 @@ $('.one-time').slick({
   nextArrow: '<button type="button" class="my-slick-next-rest"></button>',
   infinite: true,
   speed: 300,
-  slidesToShow: 1.05,
+  slidesToShow: 2,
   adaptiveHeight: true,
-  
+  variableWidth: true
 });
-
-//responsive: [
- // {
- //   breakpoint: 1024,
- //   settings: {
- //     slidesToShow: 2,
- //      }
-       
- // ]
