a171bdcd2de16f6301548fe5268397dc854e0975
annabusaga <anna.busaga@gmail.com>
2022-12-04 14:25:14 +0200

    rest_homepage
    added slider
    added buttons

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import './js/slick-for-hero';
 import './js/slick-img-rooms';
 import './js/slick-img-rooms-reverse';
 import './js/slick-rest-home';
+import './js/slick-for-restaurant';
---
+++ src/js/slick-for-restaurant.js
@@ -0,0 +1,10 @@
+$('.rest-slider').slick({
+    // dots: true,
+    prevArrow: '<button type="button" class="my-slick-prev-restaurant"></button>',
+    nextArrow: '<button type="button" class="my-slick-next-restaurant"></button>',
+    infinite: true,
+    speed: 300,
+    slidesToShow: 2,
+    adaptiveHeight: true,
+    variableWidth: true,
+});
