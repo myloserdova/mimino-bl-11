b03d22fbb904e552251f641500557cff1768549b
Anastasiia <112957753+myloserdova@users.noreply.github.com>
2022-12-05 16:54:32 +0100

    Update slick-img-rooms-reverse.js

--- src/js/slick-img-rooms-reverse.js
+++ src/js/slick-img-rooms-reverse.js
@@ -6,7 +6,7 @@ $('.room-gallery-mirror').slick({
     '<button type="button" class="my-slick-next-hotel-reverse"></button>',
   infinite: true,
   speed: 300,
-  slidesToShow: 1,
+  slidesToShow: 2,
   adaptiveHeight: true,
-  variableWidth: true,
+  variableWidth: true
 });
