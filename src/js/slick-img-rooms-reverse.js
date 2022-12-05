f63ef4189b487b4caa3a5037f34923f6f061e4dc
VasilenkoYurii <yura.vasilenko98@gmail.com>
2022-12-05 13:44:25 +0200

    Исправлен слайдер

--- src/js/slick-img-rooms-reverse.js
+++ src/js/slick-img-rooms-reverse.js
@@ -1,10 +1,12 @@
 $('.room-gallery-mirror').slick({
   // dots: true,
-  prevArrow: '<button type="button" class="my-slick-prev-hotel-reverse"></button>',
-  nextArrow: '<button type="button" class="my-slick-next-hotel-reverse"></button>',
+  prevArrow:
+    '<button type="button" class="my-slick-prev-hotel-reverse"></button>',
+  nextArrow:
+    '<button type="button" class="my-slick-next-hotel-reverse"></button>',
   infinite: true,
   speed: 300,
-  slidesToShow: 2,
+  slidesToShow: 1,
   adaptiveHeight: true,
-  variableWidth: true
+  variableWidth: true,
 });
