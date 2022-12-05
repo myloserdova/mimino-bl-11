092baff78d20ba4234b7c86e8c375fb08706a749
Oleksandr Klyn <shoni0807@gmail.com>
2022-12-05 20:23:42 +0200

    homepage-hotel fixed skick

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import './js/modal-price-choice';
 import './js/modal';
 import './js/select-choice';
 import './js/book-table-modal';
+import './js/slick-detail-rest';
 import './js/modal-price';
 import './js/phone-mask';
 import './js/slick-img-rooms-reverse';
-
 import './js/slick-for-restaurant';
-import './js/slick-detail-rest';
+
