20d2bc8e14eecf6d8944ed6fc3f125e75466208e
myloserdova <112957753+myloserdova@users.noreply.github.com>
2022-12-05 15:52:35 +0000

    Deploying to gh-pages from @ myloserdova/mimino-bl-11@9cef3005c0d892f84d2026ba7fc26f14bbf6e77a 🚀

---
+++ restaurant.436dfc4b.js
@@ -0,0 +1,2 @@
+var element=document.querySelector(".booking-table__select"),choices=new Choices(element,{searchEnabled:!1});
+//# sourceMappingURL=restaurant.436dfc4b.js.map
---
+++ restaurant.7d48492c.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(function(){var e=document.querySelector(".tel-input");IMask(e,{mask:"+{38} (000) 000-00-00"});IMask(document.querySelector(".tel-input"),{mask:"+{38} (000) 000-00-00",lazy:!1})}));
+//# sourceMappingURL=restaurant.7d48492c.js.map
