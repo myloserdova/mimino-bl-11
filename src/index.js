5f8d0ad8a13dd0f21e92311c57455a8f7bff76ed
Anastasiia <112957753+myloserdova@users.noreply.github.com>
2022-12-05 20:36:36 +0100

    Revert "js fix"

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import './js/slick-img-rooms';
 import './js/imask';
 import './js/slick-rest-home';
 import './js/modal-price-choice';   
-import './js/slick-img-rooms-reverse';
 import './js/modal';
 import './js/select-choice';
 import './js/book-table-modal';
+import './js/slick-detail-rest';
 import './js/modal-price';
 import './js/phone-mask';
+import './js/slick-img-rooms-reverse';
 import './js/slick-for-restaurant';
-import './js/slick-detail-rest';
 
